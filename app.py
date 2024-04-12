710bf01f8d883a278eb5e719a7d7cdf39494d9c7
N KARTHIK SAGAR <111840048+karthiksagarN@users.noreply.github.com>
2024-04-12 09:05:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,25 +5,20 @@ from langchain.prompts import PromptTemplate
 
 ## Function to get response form the LLama2 Model
 def getLLamaResponse(input_text, no_of_words, blog_style):
-
-    ### calling LLAMA2 MODEL
-    llm = CTransformers(model = 'llama-2-7b-chat.ggmlv3.q8_0.bin',
+    llm = CTransformers(model='/Users/karthiksagar/BlogGeneration/llama-2-7b-chat.bin',
                         model_type='llama',
-                        config={"max_new_tokens":256,
-                                'temperature':0.01})
-    ### Prompt Template
+                        config={"max_new_tokens": 256,
+                                'temperature': 0.01})
     template = """
         write a blog for {blog_style} job profile for a topic {input_text}
         within {no_of_words} words.
             """
     prompt = PromptTemplate(input_variables=["blog_style", "input_text", "no_of_words"],
                             template=template)
-    
-    ## Generate the Response form LLAMA2 Model
-    response = llm(prompt.format(blog_style= blog_style, input_text= input_text, no_of_words= no_of_words))
-    
-    print(response)
+    response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_of_words=no_of_words))
+    st.write(response)
     return response
+
 
 st.set_page_config(page_title= "Generate Blogs",
                    page_icon="🤖",
